7d62c9b3f55ec2031277051393fc258cc8558bbc
regine-tiu <regine.d.tiu@gmail.com>
2021-04-21 16:43:38 +0800

    solved

--- 04_filter_lengths.rb
+++ 04_filter_lengths.rb
@@ -4,6 +4,10 @@
 # is passed in, then 5 should be used as the length.
 
 
+def filter_lengths(strings, length = 5)
+
+strings.select {|word| word.length >= length}
+end  
 p filter_lengths(["pear", "dragonfruit", "fig", "clementine"], 4)   # => ["pear", "dragonfruit", "clementine"]
 p filter_lengths(["pear", "dragonfruit", "fig", "clementine"])      # => ["dragonfruit", "clementine"]
 p filter_lengths(["cat", "dog", "capybara", "mouse"], 7)            # => ["capybara"]
