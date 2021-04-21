899134d1b78e97a15089f4892aa4fb20fcdd0c2f
regine-tiu <regine.d.tiu@gmail.com>
2021-04-21 17:18:06 +0800

    solved

--- 07_multi_dimensional_sum.rb
+++ 07_multi_dimensional_sum.rb
@@ -1,6 +1,13 @@
 # Write a method, multi_dimensional_sum(array), that accepts a multidimensional array as an arg 
 # and returns the sum of all numbers in the array.
 
+def multi_d_sum(arr)
+ 
+new_arr = arr.flatten
+
+new_arr.sum
+
+end
 
 arr_1 = [
     [4, 3, 1],
@@ -8,11 +15,11 @@ arr_1 = [
     [2]
 ]
 
-p multi_dimensional_sum(arr_1)    # => 19
+p multi_d_sum(arr_1)    # => 19
 
 arr_2 = [
     [ [3, 6], [7] ],
     [ [5, 2], 1 ]
 ]
 
-p multi_dimensional_sum(arr_2)    # => 24
+p multi_d_sum(arr_2)    # => 24
